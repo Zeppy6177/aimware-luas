600b499bf44b4536fd6e10e4bc4d78d256287884
Zeppy6177 <62967915+Zeppy6177@users.noreply.github.com>
2021-06-27 18:28:38 -0500

    Update nadeself.lua
    Recoded and now sets fake angles

--- nadeself.lua
+++ nadeself.lua
@@ -2,46 +2,41 @@ local NadeReference = gui.Reference("MISC")
 local NadeMenuTab = gui.Tab(NadeReference, "nademenu.tab", "Self Nade")
 local NadeSelfGroupbox = gui.Groupbox(NadeMenuTab, "Nade Self", 15, 15, 200, 100)
 local tickwait = globals.TickCount()
-NadeSelfKey = gui.Keybox(NadeSelfGroupbox, "nadeself.key", "Key", 191)
-nadeval = -1
+NadeSelfKey = gui.Keybox(NadeSelfGroupbox, "nadeself.key", "Key", 6)
 
-callbacks.Register( "Draw", function()
-    lPlayer = entities.GetLocalPlayer()
-    if lPlayer == nil then return end
-    lPlayerAlive = lPlayer:IsAlive()
-    if entities.GetLocalPlayer() then
-        local lPlayerWeaponID = lPlayer:GetWeaponID();
-        if lPlayerWeaponID == 44 then
-            if NadeSelfKey:GetValue() ~= 0 then
-                if input.IsButtonPressed(NadeSelfKey:GetValue()) then
-                        ang_x = engine.GetViewAngles()["pitch"]
-                        ang_y = engine.GetViewAngles()["yaw"]
-                        ang_z = engine.GetViewAngles()["roll"]
-                        nadeval = 1
-                        engine.SetViewAngles(EulerAngles(-89,ang_y,ang_z))
-                        tickwait = globals.TickCount()
-                    elseif globals.TickCount() - tickwait > 60 then
-                        nadeval = -1
-                    elseif globals.TickCount() - tickwait > 56 then
-                        nadeval = 3
-                    elseif globals.TickCount() - tickwait > 45 then
-                        nadeval = 0
-                    elseif globals.TickCount() - tickwait > 26 then
-                        nadeval = 2
-                end
-
-                if nadeval == 1  then
-                        client.Command("+attack", true)
-                        client.Command("+attack2", true)
-                    elseif nadeval == 2 then
-                        client.Command("-attack", true)
-                    elseif nadeval == 0 then
-                        client.Command("-attack2", true)
-                    elseif nadeval == 3 then
-                        engine.SetViewAngles(EulerAngles(ang_x,ang_y,ang_z))
-                    elseif nadeval == -1 then return
-                end
-            end
-        end
-    end
-end)
+local function NadeSelf()
+    	local lPlayer = entities.GetLocalPlayer()
+    	if gui.Reference("menu"):IsActive() then return end
+    	if lPlayer == nil then return end
+    	if entities.GetLocalPlayer() then
+	   	local lPlayerWeaponID = lPlayer:GetWeaponID();
+	   	if lPlayerWeaponID == 44 then
+		  	if NadeSelfKey:GetValue() ~= 0 then
+				if input.IsButtonPressed(NadeSelfKey:GetValue()) then
+					local function nadeCmd(cmd)
+						local ang_x = engine.GetViewAngles()["pitch"]
+						local ang_y = engine.GetViewAngles()["yaw"]
+						local ang_z = engine.GetViewAngles()["roll"]
+						cmd.viewangles = EulerAngles(-89,ang_y,ang_z)
+						if globals.TickCount() - tickwait > 56 then
+							cmd.viewangles = EulerAngles(ang_x,ang_y,ang_z)
+							return
+						end
+					end
+					callbacks.Register("CreateMove", "nadeCmd", nadeCmd)
+					client.Command("+attack", true)
+					client.Command("+attack2", true)
+					tickwait = globals.TickCount()
+				elseif globals.TickCount() - tickwait > 60 then
+					callbacks.Unregister("CreateMove", "nadeCmd")
+				    	return
+				elseif globals.TickCount() - tickwait > 45 then
+				    	client.Command("-attack2", true)
+				elseif globals.TickCount() - tickwait > 26 then
+					client.Command("-attack", true)
+				end
+			end
+		end
+	end
+end
+callbacks.Register("Draw", "NadeSelf", NadeSelf)
